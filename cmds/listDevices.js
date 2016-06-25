37e76c6c7cf5837501f7256a214260a7e6fb0a9a
Ben Evans <ben@bensbit.co.uk>
2016-06-25 12:15:37 +0100

    more detailed list-devices

--- cmds/listDevices.js
+++ cmds/listDevices.js
@@ -6,6 +6,10 @@ module.exports = function listDevices (initDevice) {
 
     _.each(top.zones, function (zone) {
       console.log(zone.name)
+      Object.keys(zone).forEach((key) => {
+        console.log('  ' + key + ': ', zone[key])
+      })
+      console.log()
     })
 
   })

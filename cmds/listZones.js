bbea988ff2161d29c457a7c95979e1a392e5f71d
Ben Evans <ben@bluechimp.io>
2018-11-05 18:39:48 +0000

    style: lint fix

--- cmds/listZones.js
+++ cmds/listZones.js
@@ -2,7 +2,7 @@ var _ = require('underscore')
 
 module.exports = function listZones (initDevice) {
   initDevice.getAllGroups().then(zones => {
-    zones = Array.isArray(zones) ? zones : [zones] 
+    zones = Array.isArray(zones) ? zones : [zones]
     _.each(zones, function (devices, group) {
       console.log(devices.ID)
       _.each(devices.ZoneGroupMember, function (device) {
